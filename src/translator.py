be0aea11c3375d7d79a2cc92f3beaede463d3ff2
Bystroushaak <bystrousak@kitakitsune.org>
2014-12-09 13:12:57 +0100

    Added example of the RstToPdf() use.

--- src/translator.py
+++ src/translator.py
@@ -4,6 +4,7 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+from rst2pdf.createpdf import RstToPdf
 
 
 
@@ -16,5 +17,19 @@
 
 
 # Main program ================================================================
-if __name__ == '__main__':
-    pass
+pdf = RstToPdf(
+    language="cs",
+    font_path=[
+        "/usr/share/fonts",
+        '.',
+        '/usr/local/lib/python2.7/dist-packages/rst2pdf/fonts'
+    ],
+    stylesheets=["style.json"],
+    breaklevel=0,
+    splittables=True,
+    footer="###Page###/###Total###"
+)
+pdf.createPdf(
+    text=open("contract.rst").read(),
+    output='foo.pdf'
+)
