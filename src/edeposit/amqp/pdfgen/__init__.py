f18a688ec4bdedc897384ce24d29077dabd45f5a
Bystroushaak <bystrousak@kitakitsune.org>
2015-01-07 16:12:10 +0100

    Added reactToAMQPMessage() with reaction to GenerateContract.

--- src/edeposit/amqp/pdfgen/__init__.py
+++ src/edeposit/amqp/pdfgen/__init__.py
@@ -5,3 +5,45 @@
 #
 # Imports =====================================================================
 from translator import *
+from structures import *
+from specialization import get_contract
+
+
+# Functions & classes =========================================================
+def _instanceof(instance, class_):
+    """Check type by matching ``.__name__``."""
+    return type(instance).__name__ == class_.__name__
+
+
+def reactToAMQPMessage(message, UUID):
+    """
+    React to given (AMQP) message. `message` is usually expected to be
+    :py:func:`collections.namedtuple` structure filled with all necessary data.
+
+    Args:
+        message (\*Request class): only :class:`.ConversionRequest` class is
+                                   supported right now
+
+        UUID (str):                unique ID of received message
+
+    Returns:
+        ConversionResponse: response filled with data about conversion and\
+                            converted file.
+
+    Raises:
+        ValueError: if bad type of `message` structure is given.
+    """
+    if _instanceof(message, GenerateContract):
+        return get_contract(
+            message.firma,
+            message.pravni_forma,
+            message.sidlo,
+            message.ic,
+            message.dic,
+            message.zastoupen,
+            message.jednajici,
+        )
+
+    raise ValueError(
+        "Unknown type of request: '" + str(type(message)) + "'!"
+    )
