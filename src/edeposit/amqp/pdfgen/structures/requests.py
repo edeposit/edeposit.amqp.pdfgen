c6db5428d345774fa81396e93d0e3a444485d95f
Bystroushaak <bystrousak@kitakitsune.org>
2015-01-12 14:43:04 +0100

    Added documentation, default values to RST2PDF.

--- src/edeposit/amqp/pdfgen/structures/requests.py
+++ src/edeposit/amqp/pdfgen/structures/requests.py
@@ -15,10 +15,22 @@ class GenerateContract(namedtuple("GenerateContract", ["firma",
                                                        "dic",
                                                        "zastoupen",
                                                        "jednajici"])):
+    """
+    Request to generate contract.
+
+    Attributes:
+        firma (str): As it is defined in contract.
+        pravni_forma (str): As it is defined in contract.
+        sidlo (str): As it is defined in contract.
+        ic (str): As it is defined in contract.
+        dic (str): As it is defined in contract.
+        zastoupen (str): As it is defined in contract.
+        jednajici (str): As it is defined in contract.
+    """
     pass
 
 
-review = OrderedDict([
+review = OrderedDict([  #: This structure is used to construct GenerateReview
     ["nazev", "Název ePublikace"],
     ["podnazev", "Podnázev"],
     ["cast", "Část (svazek,díl)"],
@@ -55,4 +67,20 @@ class RST2PDF(namedtuple("RST2PDF", ["rst_content",
                                      "style",
                                      "header",
                                      "footer"])):
-    pass
+    """
+    Generic request to convert RST file to PDF.
+
+    Attributes:
+        rst_content (str): Content of the generated PDF file.
+        style (str): Style for the generated PDF file.
+        header (str, default None): Header of each page.
+        footer (str, default pagecount): Footer of each page.
+    """
+    def __new__(self, rst_content, style, header=None, footer=None):
+        return super(RST2PDF, self).__new__(
+            self,
+            rst_content,
+            style,
+            header,
+            footer
+        )
