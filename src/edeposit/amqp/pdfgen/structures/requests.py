2b693cc596167b71ca8d5d53723a456efc3c031a
Bystroushaak <bystrousak@kitakitsune.org>
2015-01-12 11:46:01 +0100

    Previous format dropped the order, this will fix it.

--- src/edeposit/amqp/pdfgen/structures/requests.py
+++ src/edeposit/amqp/pdfgen/structures/requests.py
@@ -18,33 +18,33 @@ class GenerateContract(namedtuple("GenerateContract", ["firma",
     pass
 
 
-review = OrderedDict(
-    nazev="Název ePublikace",
-    podnazev="Podnázev",
-    cast="Část (svazek,díl)",
-    nazev_casti="Název části, dílu",
-    isbn="ISBN (pokud je)",
-    isbn_souboru_publikaci="ISBN souboru (pro vícesvazkové dokumenty)",
-    generated_isbn='Přidělit agenturou ISBN',
-    author1="Autor",
-    author2="Autor 2",
-    author3="Autor 3",
-    poradi_vydani='Pořadí vydání, verze',
-    misto_vydani='Místo vydání',
-    rok_vydani="Rok vydání",
-    nakladatel_vydavatel="Nakladatel",
-    vydano_v_koedici_s='Vydáno v koedici s',
-    cena='Cena v Kč',
-    offer_to_riv='Zpřístupnit pro RIV',
-    category_for_riv="Kategorie pro RIV",
-    is_public='ePublikace je veřejná',
-    libraries_accessing="Oprávnění knihovnám",
-    libraries_that_can_access="Knihovny s přístupem k ePublikaci",
-    zpracovatel_zaznamu='Zpracovatel záznamu',
-    url="URL",
-    file="Připojit soubor s ePublikací",
-    format="Format of a file."
-)
+review = OrderedDict([
+    ["nazev", "Název ePublikace"],
+    ["podnazev", "Podnázev"],
+    ["cast", "Část (svazek,díl)"],
+    ["nazev_casti", "Název části, dílu"],
+    ["isbn", "ISBN (pokud je)"],
+    ["isbn_souboru_publikaci", "ISBN souboru (pro vícesvazkové dokumenty)"],
+    ["generated_isbn", 'Přidělit agenturou ISBN'],
+    ["author1", "Autor"],
+    ["author2", "Autor 2"],
+    ["author3", "Autor 3"],
+    ["poradi_vydani", 'Pořadí vydání, verze'],
+    ["misto_vydani", 'Místo vydání'],
+    ["rok_vydani", "Rok vydání"],
+    ["nakladatel_vydavatel", "Nakladatel"],
+    ["vydano_v_koedici_s", 'Vydáno v koedici s'],
+    ["cena", 'Cena v Kč'],
+    ["offer_to_riv", 'Zpřístupnit pro RIV'],
+    ["category_for_riv", "Kategorie pro RIV"],
+    ["is_public", 'ePublikace je veřejná'],
+    ["libraries_accessing", "Oprávnění knihovnám"],
+    ["libraries_that_can_access", "Knihovny s přístupem k ePublikaci"],
+    ["zpracovatel_zaznamu", 'Zpracovatel záznamu'],
+    ["url", "URL"],
+    ["file", "Připojit soubor s ePublikací"],
+    ["format", "Formát souboru"]
+])
 
 
 class GenerateReview(namedtuple("GenerateReview", list(review.iterkeys()))):
