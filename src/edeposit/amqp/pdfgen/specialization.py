c2c1c0e6e3fc1ade79e4a48743eabb9eae64185d
Bystroushaak <bystrousak@kitakitsune.org>
2015-01-22 15:19:50 +0100

    Fixed small bug in contract generator.

--- src/edeposit/amqp/pdfgen/specialization.py
+++ src/edeposit/amqp/pdfgen/specialization.py
@@ -56,7 +56,7 @@ def get_contract(firma, pravni_forma, sidlo, ic, dic, zastoupen, jednajici):
 
     # make sure that `firma` has its heading mark
     firma = firma.strip()
-    firma = firma + ":\n" + (len(firma) * "-")
+    firma = firma + ":\n" + ((len(firma) + 1) * "-")
 
     # patch template
     contract = Template(contract).substitute(
