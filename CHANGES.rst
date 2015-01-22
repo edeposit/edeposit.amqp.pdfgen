42d0b9a74556caccc7322f10971fbe032aaab03a
Bystroushaak <bystrousak@kitakitsune.org>
2015-01-22 15:21:24 +0100

    Fixed #16 - bug in setuptools.
    HTML generation command had to be removed.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,12 @@
 Changelog
 =========
 
+0.2.1
+-----
+    - requests_template.py renamed to requests_template.txt.
+    - Fixed #16 - bug in setuptools. HTML generation command had to be removed.
+    - Fixed small bug in contract generator.
+
 0.2.0
 -----
     - Added logo to review.
--- setup.py
+++ setup.py
@@ -9,11 +9,9 @@ import shutil
 
 from setuptools import setup, find_packages
 from distutils.command.sdist import sdist
+from distutils.filelist import FileList
 
-try:
-    from docs import getVersion
-except ImportError:  # during packaging, docs are moved to html_docs
-    from html_docs import getVersion
+from docs import getVersion
 
 
 changelog = open('CHANGES.rst').read()
@@ -22,37 +20,6 @@ long_description = "\n\n".join([
     open('CONTRIBUTORS.rst').read(),
     changelog
 ])
-
-
-class BuildSphinx(sdist):
-    """
-    Generates sphinx documentation, puts it into html_docs/, packs it to
-    package and removes unused directory.
-    """
-    def run(self):
-        d = os.path.abspath('.')
-        DOCS = d + "/" + "docs"
-        DOCS_IN = DOCS + "/_build/html"
-        DOCS_OUT = d + "/html_docs"
-
-        if not self.dry_run:
-            print "Generating the documentation .."
-
-            os.chdir(DOCS)
-            os.system("make clean")
-            os.system("make html")
-
-            if os.path.exists(DOCS_OUT):
-                shutil.rmtree(DOCS_OUT)
-
-            shutil.copytree(DOCS_IN, DOCS_OUT)
-            shutil.copy(DOCS + "/__init__.py", DOCS_OUT)  # for getVersion()
-            os.chdir(d)
-
-        sdist.run(self)
-
-        if os.path.exists(DOCS_OUT):
-            shutil.rmtree(DOCS_OUT)
 
 
 setup(
@@ -74,7 +41,7 @@ setup(
 
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    # package_data={'': ['edeposit/amqp/ltp/xslt/*.xsl']},
+    # package_data={'': ['edeposit/amqp/pdfgen/resources/*']},
 
     # scripts=[''],
 
@@ -95,6 +62,4 @@ setup(
             "sphinxcontrib-napoleon",
         ]
     },
-
-    cmdclass={'sdist': BuildSphinx}
 )
